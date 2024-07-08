2a0ca82efd602a6bad1fe39ff51a095f05255c93
Almas MD. Estiak <almas.estiak@groupebia.com>
2024-07-09 00:13:56 +0600

    Initial commit

---
+++ src/main/java/com/wsd/wsdecom/WsdEcomApplication.java
@@ -0,0 +1,13 @@
+package com.wsd.wsdecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdEcomApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WsdEcomApplication.class, args);
+  }
+
+}

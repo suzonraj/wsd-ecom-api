b4245621cf316e302cd4057f71abb9bdbe840488
Almas MD. Estiak <almas.estiak@groupebia.com>
2024-07-10 00:41:40 +0600

    Updated WishlistControllerTest with new case

--- src/test/java/com/wsd/wsdecom/WishlistControllerTest.java
+++ src/test/java/com/wsd/wsdecom/WishlistControllerTest.java
@@ -9,10 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 public class WishlistControllerTest {
@@ -23,9 +19,11 @@ public class WishlistControllerTest {
   @Test
   void testGetWishlist() throws Exception {
 //    Test case for: GET request to /api/v1/wishlist/1
-    mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/wishlist/{customerId}", 1))
+    mockMvc.perform(MockMvcRequestBuilders.get("/v1/wishlist/{customerId}", 1))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.customerId").value(1));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].customerId").value(1));
   }
 }
